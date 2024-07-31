02add036e0f5f8e75051538a545501fb5fd3f6cd
rorrofity <r.pizarro.leeson@gmail.com>
2024-07-31 19:11:19 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Personaliza la configuración aquí (si es necesario)
+  return config;
+};
